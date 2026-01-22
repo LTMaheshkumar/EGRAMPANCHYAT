1e0bdc5dc84bb0c1d0c0082e157a08bf2e45b5d2
Maheshkumar <maheshkumarjadhav2001@gmail.com>
2026-01-22 16:21:29 +0530

    Service Added

---
+++ src/main/java/com/cdac/service/CitizenService.java
@@ -0,0 +1,9 @@
+package com.cdac.service;
+
+import com.cdac.dto.CitizenRequestDto;
+import com.cdac.entities.Citizens;
+
+public interface CitizenService {
+    Citizens registerCitizen(CitizenRequestDto dto);
+}
+
